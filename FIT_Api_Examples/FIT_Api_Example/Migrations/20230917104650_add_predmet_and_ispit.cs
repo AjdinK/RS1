3daad591da26a28c9bc3c46df4a65baa28980a83
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2023-09-17 12:49:55 +0200

    v2

---
+++ FIT_Api_Examples/FIT_Api_Example/Migrations/20230917104650_add_predmet_and_ispit.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FIT_Api_Example.Migrations
+{
+    public partial class add_predmet_and_ispit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Predmet",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Predmet", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ispit",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Datum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PredmetID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ispit", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ispit_Predmet_PredmetID",
+                        column: x => x.PredmetID,
+                        principalTable: "Predmet",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ispit_PredmetID",
+                table: "Ispit",
+                column: "PredmetID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Ispit");
+
+            migrationBuilder.DropTable(
+                name: "Predmet");
+        }
+    }
+}
---
+++ FIT_Api_Examples/FIT_Api_Example/Modul1/Models/Ispit.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FIT_Api_Example.Modul1.Models
+{
+    public class Ispit
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Naziv { get; set; }
+        public DateTime Datum { get; set; }
+
+        [ForeignKey("PredmetID")]
+        public Predmet Predmet { get; set; }
+        public int PredmetID { get; set; }
+
+    }
+}
