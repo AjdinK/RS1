454c6c34b7f6385b448cbd083faa32d53f5bb6bf
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2023-09-24 17:16:34 +0200

    v4

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -8,8 +8,35 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'angular';
 
-  dugme1() {
-    alert("hello from ts file")
+  ime = "";
+  Podaci : any;
 
+  dugme1() {
+    this.ime = "ajdin V2";
   }
+
+  PreuzmiPodatke() {
+
+    let url = "https://localhost:7174/Predmet/GetAll?nazivFilter=" + this.ime ;
+    fetch(url).
+    then(
+      r => {
+
+        if (r.status !== 200) {
+          alert("Server javlja gresku : " + r.status);
+          return;
+        }
+        r.text().then(
+          t => {
+          this.Podaci = t;
+        });
+      }
+
+    ).catch(
+      err => {
+        alert("Greska u komunikaciji sa serverom : " + err);
+      }
+    );
+  }
+
 }
