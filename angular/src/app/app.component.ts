ff73ad025be78db14a5a3fb125319bbc27abf138
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2023-09-28 16:14:53 +0200

    v6

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -10,26 +10,9 @@ import {HttpClient} from "@angular/common/http";
 export class AppComponent implements OnInit{
 
   title = 'angular';
-  filterStudent = "";
-  studentPodaci:any = [];
-  odabraniStudent: any;
 
-  constructor(private httpKlijent : HttpClient) {
-  }
   ngOnInit(): void {
-       this.PreuzmiPodatke();
-  }
-  PreuzmiPodatke() {
-    this.httpKlijent.get(mojConfig.adresaServera + "/Student/GetAll?ime_prezime=" + this.filterStudent).subscribe(x => {
-      this.studentPodaci = x;
-    });
-  }
-  getPodaci() {
-    return this.studentPodaci.filter((x:any) => x.ime.toLowerCase().startsWith(this.filterStudent.toLowerCase()));
+
   }
 
-  Snimi() {
-    this.httpKlijent.post(mojConfig.adresaServera + "/Student/Snimi/",this.odabraniStudent).subscribe(x => {
-    });
-  }
 }
--- angular/src/app/student/student.component.ts
+++ angular/src/app/student/student.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {mojConfig} from "../moj_config";
 
 @Component({
   selector: 'app-student',
   templateUrl: './student.component.html',
   styleUrls: ['./student.component.css']
 })
-export class StudentComponent {
+export class StudentComponent implements OnInit{
+  filterStudent = "";
+  studentPodaci:any = [];
+  odabraniStudent: any;
 
+  constructor(private httpKlijent : HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.PreuzmiPodatke();
+  }
+  PreuzmiPodatke() {
+    this.httpKlijent.get(mojConfig.adresaServera + "/Student/GetAll?ime_prezime=" + this.filterStudent).subscribe(x => {
+      this.studentPodaci = x;
+    });
+  }
+  getPodaci() {
+    return this.studentPodaci.filter((x:any) => x.ime.toLowerCase().startsWith(this.filterStudent.toLowerCase()));
+  }
+
+  Snimi() {
+    this.httpKlijent.post(mojConfig.adresaServera + "/Student/Snimi/",this.odabraniStudent).subscribe(x => {
+    });
+  }
 }
+
+
