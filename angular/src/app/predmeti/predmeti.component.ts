1998dbcaf8ecbcd261609c73bdaf59b2f88f2a5b
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2023-10-02 13:16:58 +0200

    v6

--- angular/src/app/predmeti/predmeti.component.ts
+++ angular/src/app/predmeti/predmeti.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {mojConfig} from "../moj_config";
 
 @Component({
   selector: 'app-predmeti',
   templateUrl: './predmeti.component.html',
   styleUrls: ['./predmeti.component.css']
 })
-export class PredmetiComponent {
+export class PredmetiComponent implements OnInit{
+  podaci:any = [];
+  filterPredmeti: any = "";
 
+  constructor(private httpKlijent : HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.PreuzmiPodatke();
+  }
+  PreuzmiPodatke() {
+    //https://localhost:7174/Predmet/GetAll
+    this.httpKlijent.get(mojConfig.adresaServera + "/Predmet/GetAll?nazivFilter=" + this.filterPredmeti).subscribe(x=>{
+      this.podaci = x;
+    })
+  }
 }
