6471c76f733b4c4a5ebb9fc452d4c64da0927ba2
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2023-10-12 15:56:18 +0200

    v8

--- webapi/FIT_Api_Examples/Modul3_MaticnaKnjiga/ViewModels/MaticnaKnjigaDetaljiUpisiVM.cs
+++ webapi/FIT_Api_Examples/Modul3_MaticnaKnjiga/ViewModels/MaticnaKnjigaDetaljiUpisiVM.cs
@@ -5,7 +5,9 @@ namespace FIT_Api_Examples.Modul3_MaticnaKnjiga.ViewModels
 {
     public class MaticnaKnjigaDetaljiUpisiVM
     {
-    
-
+        public object upisAkademskeGodineId { get; internal set; }
+        public object akademskaGodinaOpis { get; internal set; }
+        public object godinaStudija { get; internal set; }
+        public object obnova { get; internal set; }
     }
 }
