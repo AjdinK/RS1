f0f0b9ead9ba633219e9c327e797e0ba88d0d627
Ajdin Kuduzovic <Ajdin.kuduzovic@gmail.com>
2023-12-03 12:56:09 +0100

    v18

---
+++ FIT_Api_Examples/FIT_Api_Example/Modul1/Models/Obavijest.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using FIT_Api_Example.Modul0_Autentifikacija.Models;
+
+namespace FIT_Api_Example.Modul1.Models
+{
+    public class Obavijest
+    {
+
+        [Key]
+        public int ID {get;set;}
+        public string Naslov  {get;set;}
+        public string Tekst {get;set;}
+        public DateTime DatumKreiranja { get; set; }
+
+     
+       
+        public int CreatedByKorisnikID { get; set; }
+        [ForeignKey(nameof(CreatedByKorisnikID))]
+        public KorisnickiNalog EvidentiraoKorisnik { get; set; }
+
+
+        [ForeignKey(nameof(IzmijenioKorisnik))]
+        public int? IzmijenioKorisnikID { get; set; }
+        public KorisnickiNalog IzmijenioKorisnik { get; set; }
+        
+        
+        public DateTime? DatumUpdate  { get; set; }
+
+    }
+}
